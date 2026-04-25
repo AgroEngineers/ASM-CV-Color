22220d96748cbcf07cac0c355e98817814bda36d
Robo_Start <craft.surkov.roma@gmail.com>
2026-04-25 22:06:27 +0300

    Update to latest api version

--- color.py
+++ color.py
@@ -2,8 +2,31 @@ from typing import Union
 
 import numpy
 from asm.api.base import ModuleTask, ModuleTaskInput, ModuleTaskOutput, ModuleConfiguration, ModuleInformation, \
-    ContainerParameterResults, ContainerParameter, ContainerParameterType
+    ContainerParameterResults, ContainerParameter, ContainerParameterType, ContainerParameterGroup, \
+    ContainerParameterGroupWebSpec, ContainerParameterWebSpecAlignment
 from asm.api.cv import ASMOpenCV, FrameType
+
+color_group: ContainerParameterGroup = ContainerParameterGroup(
+    "Color",
+    ContainerParameterType.RANGE,
+    ContainerParameterGroupWebSpec(
+        html="""
+            <div id="parameter-color-box" style="background-color: rgb(%cv_color_cv-color_color_r%, %cv_color_cv-color_color_g%, %cv_color_cv-color_color_b%)" class="parameter-color-box"></div>
+        """,
+        css="""
+            .parameter-color-box {
+                margin-right: 10px;
+                width: 10%;
+                height: 40%;
+                border: 2px solid #5e5e5e;
+            }
+        """,
+        alignment=ContainerParameterWebSpecAlignment.BEFORE
+    )
+)
+parameters: list[ContainerParameter] = [ContainerParameter(name="R", group=color_group),
+                                        ContainerParameter(name="G", group=color_group),
+                                        ContainerParameter(name="B", group=color_group)]
 
 
 class Color(ASMOpenCV):
@@ -11,16 +34,17 @@ class Color(ASMOpenCV):
         return FrameType.OBJECT
 
     def module_info(self) -> ModuleInformation:
-        return ModuleInformation(name="CV-Color", version="1.0.0",
-                                 parameters=[ContainerParameter(name="R", parameter_type=ContainerParameterType.RANGE),
-                                             ContainerParameter(name="G", parameter_type=ContainerParameterType.RANGE),
-                                             ContainerParameter(name="B", parameter_type=ContainerParameterType.RANGE)])
+        return ModuleInformation(
+            name="CV-Color",
+            version="1.0.0",
+            parameters=parameters
+        )
 
     def process(self, frame: numpy.ndarray) -> list[ContainerParameterResults]:
         b, g, r = frame.mean(axis=0)
-        return [ContainerParameterResults(name="R", result=r),
-                ContainerParameterResults(name="G", result=g),
-                ContainerParameterResults(name="B", result=b)]
+        return [ContainerParameterResults(parameter=parameters[0], result=r),
+                ContainerParameterResults(parameter=parameters[1], result=g),
+                ContainerParameterResults(parameter=parameters[2], result=b)]
 
     def configuration(self, configuration: ModuleConfiguration):
         return None
